c0b5adb6aac113f8db2cb17744a99646e6cb1785
JonathanVCB <viniciusjonathan@hotmail.com>
2022-12-02 20:33:53 -0300

    finish

--- crud.sql
+++ crud.sql
@@ -4,14 +4,28 @@
 -- Criação
 
 -- 1)
-
+INSERT INTO clientes
+  (nome, lealdade)
+VALUES
+  ('Georgia', 0);
 
 
 -- 2)
-
+ INSERT INTO pedidos
+    (status, cliente_id)
+VALUES
+  ('Recebido', 11);
 
 
 -- 3)
+ INSERT INTO produtos_pedidos
+  (pedido_id, produto_id)
+VALUES
+  (6, 1),
+  (6, 2),
+  (6, 6),
+  (6, 8),
+    (6, 8);
 
 
 
@@ -19,16 +33,38 @@
 
 -- 1)
 
-
+  SELECT
+clientes.id,
+clientes.nome,
+clientes.lealdade,
+pedidos.id,
+pedidos.status,
+pedidos.cliente_id,
+produtos.id,
+produtos.nome,
+produtos.tipo,
+round(produtos.preço:: numeric,2)
+	AS preço,
+produtos.pts_de_lealdade
+FROM
+pedidos 
+JOIN  clientes ON pedidos.cliente_id = clientes.id
+JOIN  produtos_pedidos pp ON pp.pedido_id = pedidos.id
+JOIN produtos ON produtos.id = pp.produto_id
+WHERE clientes.nome = 'Georgia'
 
 -- Atualização
 
 -- 1)
 
-
+UPDATE 
+clientes 
+SET lealdade = 48
+WHERE id = 11
+RETURNING  *
 
 -- Deleção
 
 -- 1)
-
+DELETE FROM clientes WHERE id = 4 RETURNING *
 
--- selects.sql
+++ selects.sql
@@ -2,11 +2,52 @@
 -- Seleções de dados
 
 -- 1)
-
+ SELECT
+ pedidos.*, produtos.id, produtos.nome, produtos.tipo, round(produtos.preço:: numeric,2)
+	AS preço, produtos.pts_de_lealdade
+ FROM 
+ pedidos
+ JOIN
+ produtos_pedidos ON pedidos.id= produtos_pedidos.pedido_id
+ JOIN
+ produtos ON produtos.id = produtos_pedidos.produto_id;
+ 
 -- 2)
+SELECT
+produtos.id
+FROM
+pedidos 
+JOIN produtos_pedidos ON pedidos.id = produtos_pedidos.pedido_id
+JOIN produtos ON produtos.id = produtos_pedidos.produto_id
+WHERE produtos.nome = 'Fritas'
 
 -- 3)
+SELECT
+clientes.nome
+FROM
+pedidos 
+JOIN produtos_pedidos ON pedidos.id = produtos_pedidos.pedido_id
+JOIN produtos ON produtos.id = produtos_pedidos.produto_id
+JOIN clientes ON clientes.id = pedidos.cliente_id
+WHERE produtos.nome = 'Fritas'
 
 -- 4)
+SELECT
+sum(round(produtos.preço:: numeric,2)
+	) 
+FROM
+pedidos 
+JOIN  clientes ON pedidos.cliente_id = clientes.id
+JOIN  produtos_pedidos pp ON pp.pedido_id = pedidos.id
+JOIN produtos ON produtos.id = pp.produto_id
+WHERE clientes.nome = 'Laura'
 
 -- 5)
+SELECT
+produtos.nome, count(produtos.nome) 
+FROM
+produtos
+JOIN produtos_pedidos pp ON pp.produto_id = produtos.id
+GROUP  BY produtos.nome
+ORDER BY produtos.nome
+
